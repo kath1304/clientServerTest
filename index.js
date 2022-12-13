3f4216b0fac8c1d080f81a05e59ba8cc7cab2325
belorin <valeriociccio@yahoo.it>
2022-12-13 16:45:53 +0100

    primo upload vale

--- index.js
+++ index.js
@@ -27,6 +27,23 @@ app.delete('/:a', (req, res, _next) =>{
 })
 
 /*PUT usato per modificare il valore di un elemento cercandolo per parametro*/
-app.put('/:a', (req, res, _next) =>{
-
+app.put('/:old/:new', (req, res, _next) =>{
+    if(utilities.length === 0) {
+        res.json('DB vuoto')
+        return
+    }
+    console.log(req.params)
+    console.log(req.params['old'])
+    for(let i = 0; i < utilities.length; i++){
+        console.log(utilities[i].a)
+        if(utilities[i].a == req.params['old']) {
+            console.log('Sosituisco ' + utilities[i].a)
+            utilities[i].a = req.params['new']
+            console.log(' con ' + req.params['new'])
+            console.log(utilities)
+            res.json('Sostituito ' + req.params['old'] + ' con ' + req.params['new'])
+            return
+        }
+    }
+    res.json('Non esiste')
 })
